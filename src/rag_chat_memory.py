fb01ddbd9bd6ccdf959ff57b3c6ba4dc28b38084
sudheersaladhi1-stack <sudheer.saladhi1@gmail.com>
2026-02-09 14:45:34 +0530

    Remove uv.lock so Streamlit uses requirements.txt

--- src/rag_chat_memory.py
+++ src/rag_chat_memory.py
@@ -7,7 +7,7 @@ from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnablePassthrough, RunnableLambda
 from langchain_core.runnables.history import RunnableWithMessageHistory
 
-from langchain.chat_models import ChatOpenAI
+from langchain_community.chat_models import ChatOpenAI
 import os
 from dotenv import load_dotenv
 
