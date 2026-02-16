44d00bf4b5e5a85c6ffe4b937aac0310855cf493
sudheersaladhi1-stack <sudheer.saladhi1@gmail.com>
2026-02-16 12:50:56 +0530

    chunks

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from langchain_community.vectorstores import Chroma
 from langchain_community.embeddings import SentenceTransformerEmbeddings
 from langchain_core.documents import Document
 
-from src.rag_chat_memory import rag_chain_with_memory, store
+from src.rag_chat_memory import rag_chain_memory, store
 from src.rag_chain import rag_chain
 
 
